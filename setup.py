f5a55f283b8606550d10ffcab841b045b567c919
timliao <bonis0324work@gmail.com>
2025-01-06 16:18:42 +0800

    build: modify setup and requirement
    Modify setup.py and requirements_py2.txt for supporting redis-py 2.10.0+.

--- setup.py
+++ setup.py
@@ -41,31 +41,31 @@ setup(
     packages=find_packages(where="app"),
     package_dir={"": "app"},
     install_requires=[
-        "dictor >= 0.1.12, < 1.0",
-        "six >= 1.16.0, < 2.0",
+        "dictor < 1.0",
+        "six < 2.0",
         "tzlocal == 2.1;python_version == '2.7'",
         "apscheduler == 3.8.1;python_version == '2.7'",
-        "apscheduler >= 3.10.4, < 4.0;python_version >= '3.7'",
+        "apscheduler < 4.0;python_version >= '3.7'",
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*, !=3.6.*",
     extras_require={
         "redis": [
             "python-redis-lock == 3.7.0;python_version == '2.7'",
-            "python-redis-lock >= 4.0.0, < 5.0;python_version >= '3.7'",
+            "python-redis-lock < 5.0;python_version >= '3.7'",
             "hiredis == 1.1.0;python_version == '2.7'",
-            "redis == 3.5.3;python_version == '2.7'",
-            "redis[hiredis] >= 5.1.1, < 6.0;python_version >= '3.7'",
+            "redis >= 2.10.0, <= 3.5.3;python_version == '2.7'",
+            "redis[hiredis] < 6.0;python_version >= '3.7'",
         ],
         "memory": [
-            "sortedcontainers >= 2.4.0, < 3.0",
+            "sortedcontainers < 3.0",
         ],
         "mongodb": [
             "pymongo == 3.13.0;python_version == '2.7'",
-            "pymongo >= 3.0, < 5.0;python_version >= '3.7'",
+            "pymongo < 5.0;python_version >= '3.7'",
         ],
         "gevent": [
             "apscheduler[gevent] == 3.8.1;python_version == '2.7'",
-            "apscheduler[gevent] >= 3.10.4, < 4.0;python_version >= '3.7'",
+            "apscheduler[gevent] < 4.0;python_version >= '3.7'",
         ],
     },
 )
