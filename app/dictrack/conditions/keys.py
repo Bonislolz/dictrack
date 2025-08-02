932a5aa7783fbba8bb447da24356f9b0b216f3dd
timliao <bonis0324work@gmail.com>
2025-08-02 16:34:58 +0800

    feat: add KeyValueInList and related classes for list-based checks

--- app/dictrack/conditions/keys.py
+++ app/dictrack/conditions/keys.py
@@ -216,3 +216,173 @@ class KeyValueNotContained(KeyValueContained):
             return False
 
         return not KeyValueContained.check(self, data, *args, **kwargs)
+
+
+class KeyValueInList(KeyExists):
+    def __init__(self, key, value, *args, **kwargs):
+        super(KeyValueInList, self).__init__(key, *args, **kwargs)
+
+        valid_type(value, (list, tuple))
+        self._value = value
+
+    def __eq__(self, other):
+        return self.key == other.key and self.value == other.value
+
+    def __hash__(self):
+        key_hash = hash(self.key)
+        value_hash = hash(tuple(self.value))
+
+        return hash(str(key_hash) + str(value_hash))
+
+    def __repr__(self):
+        return "<KeyValueInList (key={} value={})>".format(self.key, self.value)
+
+    def __getstate__(self):
+        state = super(KeyValueInList, self).__getstate__()
+        state["value"] = self.value
+
+        return state
+
+    def __setstate__(self, state):
+        super(KeyValueInList, self).__setstate__(state)
+        self._value = state["value"]
+
+    @property
+    def value(self):
+        return self._value
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not super(KeyValueInList, self).check(data, *args, **kwargs):
+            return False
+
+        result = dictor(data, self.key)
+
+        return result in self.value
+
+
+class KeyValueNotInList(KeyValueInList):
+    def __repr__(self):
+        base_repr = super(KeyValueNotInList, self).__repr__()
+        return base_repr.replace("KeyValueInList", self.__class__.__name__)
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not KeyExists.check(self, data, *args, **kwargs):
+            return False
+
+        return not KeyValueInList.check(self, data, *args, **kwargs)
+
+
+class KeyValueListHasItem(KeyExists):
+    def __init__(self, key, value, *args, **kwargs):
+        super(KeyValueListHasItem, self).__init__(key, *args, **kwargs)
+
+        valid_type(value, (int, str))
+        self._value = value
+
+    def __eq__(self, other):
+        return self.key == other.key and self.value == other.value
+
+    def __hash__(self):
+        key_hash = hash(self.key)
+        value_hash = hash(self.value)
+
+        return hash(str(key_hash) + str(value_hash))
+
+    def __repr__(self):
+        return "<KeyValueListHasItem (key={} value={})>".format(self.key, self.value)
+
+    def __getstate__(self):
+        state = super(KeyValueListHasItem, self).__getstate__()
+        state["value"] = self.value
+
+        return state
+
+    def __setstate__(self, state):
+        super(KeyValueListHasItem, self).__setstate__(state)
+        self._value = state["value"]
+
+    @property
+    def value(self):
+        return self._value
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not super(KeyValueListHasItem, self).check(data, *args, **kwargs):
+            return False
+
+        result = dictor(data, self.key)
+
+        return self.value in result
+
+
+class KeyValueListNotHasItem(KeyValueListHasItem):
+    def __repr__(self):
+        base_repr = super(KeyValueListNotHasItem, self).__repr__()
+        return base_repr.replace("KeyValueListHasItem", self.__class__.__name__)
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not KeyExists.check(self, data, *args, **kwargs):
+            return False
+
+        return not KeyValueListHasItem.check(self, data, *args, **kwargs)
+
+
+class KeyValueListIntersectList(KeyExists):
+    def __init__(self, key, value, *args, **kwargs):
+        super(KeyValueListIntersectList, self).__init__(key, *args, **kwargs)
+
+        valid_type(value, (list, tuple))
+        self._value = value
+
+    def __eq__(self, other):
+        return self.key == other.key and self.value == other.value
+
+    def __hash__(self):
+        key_hash = hash(self.key)
+        value_hash = hash(tuple(self.value))
+
+        return hash(str(key_hash) + str(value_hash))
+
+    def __repr__(self):
+        return "<KeyValueListIntersectList (key={} value={})>".format(
+            self.key, self.value
+        )
+
+    def __getstate__(self):
+        state = super(KeyValueListIntersectList, self).__getstate__()
+        state["value"] = self.value
+
+        return state
+
+    def __setstate__(self, state):
+        super(KeyValueListIntersectList, self).__setstate__(state)
+        self._value = state["value"]
+
+    @property
+    def value(self):
+        return self._value
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not super(KeyValueListIntersectList, self).check(data, *args, **kwargs):
+            return False
+
+        result = dictor(data, self.key)
+
+        return bool(set(self.value) & set(result))
+
+
+class KeyValueListNotIntersectList(KeyValueListIntersectList):
+    def __repr__(self):
+        base_repr = super(KeyValueListNotIntersectList, self).__repr__()
+        return base_repr.replace("KeyValueListIntersectList", self.__class__.__name__)
+
+    @typecheck()
+    def check(self, data, *args, **kwargs):
+        if not KeyExists.check(self, data, *args, **kwargs):
+            return False
+
+        return not KeyValueListIntersectList.check(self, data, *args, **kwargs)
