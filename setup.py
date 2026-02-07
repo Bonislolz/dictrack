d8e058dce3b75b985ff89cb8d12871f933d9a1e5
Ying Xiang <f416720001@yahoo.com.tw>
2026-02-07 20:49:12 +0800

    ci: add Github Actions workflows and update setup.py

--- setup.py
+++ setup.py
@@ -68,5 +68,21 @@ setup(
             "apscheduler[gevent] == 3.8.1;python_version == '2.7'",
             "apscheduler[gevent] < 4.0;python_version >= '3.7'",
         ],
+        "docs": [
+            "sphinx",
+            "sphinxawesome-theme",
+        ],
     },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ],
 )
